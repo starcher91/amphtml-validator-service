68fb5b542dd2c0801236e2c4c38c3103f9926e43
Stephen Archer <starcher91@gmail.com>
2017-06-03 17:06:38 -0400

    Refactored index to pull in pages to validate from a config file, and moved validation output to console so requests respond quickly

--- server/index.js
+++ server/index.js
@@ -1,53 +1,61 @@
 const express = require('express');
 const app = express();
 const ampHtmlValidator = require("amphtml-validator");
-const https = require('https');
+const https = require("https");
+let pages = require("./config/amppages.json");
 
 app.get('/', function (req, res) {
-
-  ampHtmlValidator.getInstance().then(function(validator) {
-    var options = {
-      host: 'www.cnet.com',
-      port: "443",
-      path: '/google-amp/news/apple-wwdc-developers-google-facebook-microsoft-amazon-ar-ai-siri/?setDevice=mobile'
-    };
-
-    var cnetReq = https.get(options, function(cnetRes) {
-      var body = "";
-
-      let { statusCode } = cnetRes;
-
-      if (statusCode != 200) {
-        res.resume();
-        return;
-      }
-
-      cnetRes.on('data', function(chunk) {
-        body += chunk
-      }).on('end', function() {
-        var result = validator.validateString(body);
-
-        if (result.status !== "PASS") {
-          var errorText = ""
-          for (var i = 0; i < result.errors.length; i++) {
-            var error = result.errors[i];
-            var msg = 'line ' + error.line + ', col ' + error.col + ': ' + error.message;
-            if (error.specUrl !== null) {
-              msg += ' (see ' + error.specUrl + ')';
-            }
-          errorText += msg + "\n";
-          }
-          res.send(errorText);
-        }
-
-        res.send(result.status);
-      }).on('error', function(error) {
-        res.send(error);
-      });
+    res.send();
+    pages.forEach(function(item, i) {
+        pageRetriever(pages[i], i);
     });
-  });
 });
 
+var pageRetriever = function (options, i) {
+    var cnetReq = https.get(options, function (cnetRes) {
+        var body = "";
+
+        let { statusCode } = cnetRes;
+
+        if (statusCode != 200) {
+            res.resume();
+            return;
+        }
+
+        cnetRes.on('data', function (chunk) {
+            body += chunk;
+        }).on('end', function () {
+            pages[i].body = body;
+            validate(body, i);
+        }).on('error', function (error) {
+            console.log(error);
+        });
+    });
+};
+
+var validate = function (body, i) {
+    ampHtmlValidator.getInstance().then(function (validator) {
+        var result = validator.validateString(body);
+
+        var errorText = ""
+        if (result.status !== "PASS") {
+            for (var j = 0; j < result.errors.length; j++) {
+                var error = result.errors[j];
+                var msg = 'line ' + error.line + ', col ' + error.col + ': ' + error.message;
+                if (error.specUrl !== null) {
+                    msg += ' (see ' + error.specUrl + ')';
+                }
+                errorText += msg + "\n";
+            }
+        }
+        
+        console.log(pages[i].path + " - " + result.status);
+        if (errorText) {
+            console.log(errorText);
+        }
+    });
+};
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
