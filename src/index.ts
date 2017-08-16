aab34c3e64a0a432d40667b5f35bd78454d6a96d
Stephen Archer <starcher91@gmail.com>
2017-08-16 00:30:18 -0400

    Moving notification sending out to Notifier service

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,27 @@
 import * as express from "express";
-const app = express();
-import * as ampHtmlValidator from "amphtml-validator";
-import * as rp from "request-promise";
-import * as request from "request";
 import * as config from "./config";
-import { AlertFactory } from "./alerts/AlertFactory";
 import { Validator } from "./services/Validator";
+import { Notifier } from "./services/Notifier";
 
-let state = {
-    validationErrors: [],
-    validationPasses: []
-};
-
-if (!config) {
-    console.log("You need to define a config file for the service to work");
-    throw new Error();
-}
+const app = express();
 
 app.get("/", function(req, res) {
     //go ahead and send the response so the requester isn't waiting
     res.send();
+
+    //error out if no config setup
+    if (!config) {
+        console.log("You need to define a config file for the service to work");
+        throw new Error();
+    }
+
+    //set up state
+    let state = {
+        validationErrors: [],
+        validationPasses: []
+    };
+    
+    //for each page in config, retrieve and validate the HTML, then send alerts if necessary
     let validator = new Validator();
     config.pages.forEach(function(item, i) {
         validator.validate(item).then(function(validationResult) {
@@ -29,34 +31,14 @@ app.get("/", function(req, res) {
                 state.validationPasses.push({ "result": validationResult, "page": item });
             }
             
-            //validated all pages sent and there are errors, send notifications
+            //validated all pages sent and there are errors, send alerts
             if (state.validationErrors.length != 0 && state.validationErrors.length + state.validationPasses.length == config.pages.length) {
-                sendNotifications(state.validationErrors);
+                let notifier = new Notifier();
+                notifier.sendAlerts(config.alerts, state.validationErrors);
             }
         });
     });
 });
-
-var sendNotifications = function(results) {
-    config.alerts.forEach(function(item, i) {
-        let alert = AlertFactory.getAlert(item, { "results": results });
-
-        if (alert) {
-            request(alert.post_options, function(error, response, body) {
-                if (error) {
-                    console.log(error);
-                }
-
-                if (response && response.statusCode != 200) {
-                    console.log("Error sending alert. ${response.statusCode}")
-                    return;
-                }
-            });
-        } else {
-            throw new Error("Failure getting alert for alert url ${item.url}. Is there an alert for that type?")
-        }
-    });
-}
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
